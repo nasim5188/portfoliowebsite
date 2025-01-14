ac35f6bfec030365082b801414bdfb8ab4ea7174
nasim5188 <nasim.ahamed411@gmail.com>
2025-01-14 23:28:31 +0600

    Update contact & footer section

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,14 +5,14 @@
       <img src="assets/img/my-profile-img.jpg" alt="" class="img-fluid rounded-circle">
     </div>
 
-    <a href="index.html" class="logo d-flex align-items-center justify-content-center">
+    <a href="" class="logo d-flex align-items-center justify-content-center">
       <!-- Uncomment the line below if you also wish to use an image logo -->
       <!-- <img src="assets/img/logo.png" alt=""> -->
       <h1 class="sitename">Nasim Ahamed</h1>
     </a>
 
     <div class="social-links text-center">
-      <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+      {{-- <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a> --}}
       <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
       <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
       <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
